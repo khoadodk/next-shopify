77ed6dd532c36aa2dbcbafa5c586dd49b6f695c1
DESKTOP-NPJ9KD5\DK <khoado.dk@gmail.com>
2022-06-15 20:05:54 -0400

    Added SWR data fetching

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,10 +1,44 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { formatter } from '../utils/helpers';
 import ProductOptions from './ProductOptions';
 import { CartContext } from '../context/shopContext';
+import useSWR from 'swr';
+import axios from 'axios';
+
+// Data Fetching
+const fetcher = (url, id) =>
+  axios
+    .get(url, {
+      params: {
+        id,
+      },
+    })
+    .then((res) => res.data);
 
 export default function ProductForm({ product }) {
+  // SWR hook
+  const { data: productInventory } = useSWR(
+    ['/api/available', product.handle],
+    (url, id) => fetcher(url, id),
+    { errorRetryCount: 3 }
+  );
+
+  const [available, setAvailable] = useState(true);
+
   const { addToCart } = useContext(CartContext);
+
+  useEffect(() => {
+    if (productInventory) {
+      const checkAvailable = productInventory?.variants.edges.filter(
+        (item) => item.node.id === selectedVariant.id
+      );
+      if (checkAvailable[0].node.availableForSale) {
+        setAvailable(true);
+      } else {
+        setAvailable(false);
+      }
+    }
+  });
 
   // Dynamic variant based on option
   const allVariantOptions = product.variants.edges?.map((variant) => {
@@ -75,12 +109,18 @@ export default function ProductForm({ product }) {
           setOptions={setOptions}
         />
       ))}
-      <button
-        className="bg-black rounded-lg text-white px-2 py-3 mt-3 hover:bg-gray-800"
-        onClick={() => addToCart(selectedVariant)}
-      >
-        Add to Cart
-      </button>
+      {available ? (
+        <button
+          className="bg-black rounded-lg text-white px-2 py-3 mt-3 hover:bg-gray-800"
+          onClick={() => addToCart(selectedVariant)}
+        >
+          Add to Cart
+        </button>
+      ) : (
+        <button className="rounded-lg text-white px-2 py-3 mt-3 bg-red-800 cursor-not-allowed">
+          Sold out!
+        </button>
+      )}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,31 @@
-import { getProductsInCollection } from '../lib/shopify'
+import { getProductsInCollection } from '../lib/shopify';
 import ProductList from '../components/ProductList';
+import Hero from '../components/Hero';
+import Head from 'next/head';
 
-export default function Home({products}) {
+export default function Home({ products }) {
   return (
-    <div className='text-3xl'>
+    <div className="">
+      <Head>
+        <title>Headless Shopify</title>
+        <meta
+          httpEquiv="Content-Type"
+          content="text/html; charset=utf-8"
+        />
+        <meta
+          httpEquiv="Content-Type"
+          content="text/html; charset=ISO-8859-1"
+        />
+      </Head>
+      <Hero />
       <ProductList products={products} />
     </div>
-  )
+  );
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   const products = await getProductsInCollection();
   return {
-    props: {products}
-  }
+    props: { products },
+  };
 }
